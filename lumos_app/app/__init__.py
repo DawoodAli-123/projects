5f05e4aa64431863c6abdc7323e292e6ed006170
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-11 23:24:00 +0530

    Refactor imports in __init__.py

--- lumos_app/app/__init__.py
+++ lumos_app/app/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, render_template, redirect, url_for, session
-import configparser
+from flask import Flask
 from config import Config
+from extensions import db, jwt, migrate
+from .routes.auth import auth_bp
+from .routes.user import user_bp
 import git
 import os
 import psycopg2
 from psycopg2 import pool
+import configparser
 
 app = Flask(__name__)
