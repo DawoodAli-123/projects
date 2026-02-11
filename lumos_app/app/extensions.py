a41db39ca277ef11de0b8c28151699cbe199d7af
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-11 22:39:41 +0530

    Set up database and authentication extensions
    Initialize SQLAlchemy, JWTManager, and Migrate instances.

--- lumos_app/app/extensions.py
+++ lumos_app/app/extensions.py
@@ -1,1 +1,9 @@
 # db, jwt, migrate, etc. related code will store here
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+
