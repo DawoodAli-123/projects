1aa24f59af01e6ded8bffc509ee5bb849e4b0882
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-13 00:25:08 +0530

    Register multiple blueprints in the app routes

--- lumos_app/app/routes/__init__.py
+++ lumos_app/app/routes/__init__.py
@@ -1,1 +1,22 @@
+from .testcases import testcases_bp
+from .testblocks import testblocks_bp
+from .testpacks import testpacks_bp
+from .testelements import testelements_bp
+from .test_configurations import configurations_bp
+from .testexecutions import executions_bp
+from .testreports import testreports_bp
+from .user_details import user_bp
+from .userstory import userstory_bp
+from .ut_cst import utcst_bp
 
+def register_blueprints(app):
+    app.register_blueprint(testcases_bp, url_prefix="/testcases")
+    app.register_blueprint(testblocks_bp, url_prefix="/testblocks")
+    app.register_blueprint(testpacks_bp, url_prefix="/testpacks")
+    app.register_blueprint(testelements_bp, url_prefix="/testelements")
+    app.register_blueprint(configurations_bp, url_prefix="/configurations")
+    app.register_blueprint(executions_bp, url_prefix="/executions")
+    app.register_blueprint(testreports_bp, url_prefix="/testreports")
+    app.register_blueprint(user_bp, url_prefix="/users")
+    app.register_blueprint(userstory_bp, url_prefix="/user-stories")
+    app.register_blueprint(utcst_bp, url_prefix="/ut-cst")
