9807dc849a299332e38dd2f50cf413671500d991
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-13 01:03:42 +0530

    Implement PostgreSQL connection pool in extensions.py
    Added a PostgreSQL connection pool for database management.

--- lumos_app/app/extensions.py
+++ lumos_app/app/extensions.py
@@ -1,9 +1,24 @@
 # db, jwt, migrate, etc. related code will store here
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from psycopg2 import pool
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate()
+
+connection_pool = None
+
+def init_db_pool(db_config):
+    global connection_pool
+
+    connection_pool = pool.ThreadedConnectionPool(
+        1,
+        20,
+        user=db_config["user"],
+        password=db_config["password"],
+        host=db_config["host"],
+        port=db_config["port"],
+        database=db_config["dbname"]
+    )
 
