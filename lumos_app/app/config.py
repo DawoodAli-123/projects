4ec0ce9e0636e2bb53ae974bc8b289892e85bb88
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-13 01:02:25 +0530

    Implement load_db_config function
    Add a function to load database configuration from config.ini

--- lumos_app/app/config.py
+++ lumos_app/app/config.py
@@ -1,1 +1,22 @@
+import configparser
+import os
 
+def load_db_config():
+    config_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "config.ini"
+    )
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    active_db = config['LUMOS_DB']['active_db']
+    db_config = config[active_db]
+
+    return {
+        "user": db_config["user"],
+        "password": db_config["password"],
+        "host": db_config["host"],
+        "port": db_config["port"],
+        "dbname": db_config["dbname"]
+    }
