29b82b14018baf9cdb66b122dbd905e0833e8902
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-13 00:50:08 +0530

    Refactor app initialization and add extensions
    Refactored app initialization into create_app function and added extensions.

--- lumos_app/app/__init__.py
+++ lumos_app/app/__init__.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from config import Config
+from extensions import db, jwt, migrate
+from routes import register_blueprints
 import git
 import os
 import psycopg2
 from psycopg2 import pool
-import configparser
 
-app = Flask(__name__)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+
+    # Load config
+    app.config.from_object(config_class)
+
+    # Init extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register blueprints
+    register_blueprints(app)
+
+    return app
+
+
