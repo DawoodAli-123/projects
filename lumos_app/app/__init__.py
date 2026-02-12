269ef656002e0477ca81864f9ce420004e380ad7
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-12 23:01:09 +0530

    Update __init__.py

--- lumos_app/app/__init__.py
+++ lumos_app/app/__init__.py
@@ -1,16 +1,5 @@
 from flask import Flask
 from config import Config
-from extensions import db, jwt, migrate
-from .routes.testcases import testcases_bp
-from .routes.testblocks import testblocks_bp
-from .routes.testpacks import testpacks_bp
-from .routes.testelements import testelements_bp
-from .routes.test_configurations import configurations_bp
-from .routes.testexecutions import executions_bp
-from .routes.testreports import testreports_bp
-from .routes.user_details import user_bp
-from .routes.userstory import userstory_bp
-from .routes.ut_cst import utcst_bp
 import git
 import os
 import psycopg2
