39cc7e98da0324ea7f24f6a96c5dfdfe4872c916
Dawood Ali Shaik <dawoodalishaik02@gmail.com>
2026-02-17 23:17:12 +0530

    Add DB config loader and app config class
    Added database and application configuration loading functions.

--- lumos_app/app/config.py
+++ lumos_app/app/config.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 
+# --------- DB CONFIG LOADER ----------
 def load_db_config():
     config_path = os.path.join(
         os.path.dirname(os.path.dirname(__file__)),
@@ -20,3 +21,7 @@ def load_db_config():
         "port": db_config["port"],
         "dbname": db_config["dbname"]
     }
+
+# --------- APP CONFIG ----------
+class Config:
+    BASE_DIR = os.path.abspath("safe/config/files")
